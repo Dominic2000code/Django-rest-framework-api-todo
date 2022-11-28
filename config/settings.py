baabf5612d79d8e74a8f55bc87f9bc87ae1e2c1f
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-11-28 09:59:15 +0000

    added django-rest-framework settings

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party apps
+    'rest_framework',
 
     # local apps
     'todos',
@@ -122,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
